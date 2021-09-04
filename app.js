2caca5e180fdcc913f572df494f9dc8ba88498a9
Alexfkr9 <alexfkr9@gmail.com>
2021-09-04 12:22:08 +0300

    new branch

--- app.js
+++ app.js
@@ -1,108 +1,120 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const Schema = mongoose.Schema;
+const fs = require("fs");
+    
 const app = express();
 const jsonParser = express.json();
-const fs = require('fs');
-const sharp = require('sharp');   // crop img
-
-
-app.use(express.static(__dirname + '/data')); // folder for edited user's photo
-
-
-// Model DB --------------------------------------------
-const userScheme = new Schema({
-                    name: String,                    
-                    surname: String,
-                    email: String,
-                    photo: String
-                    },
-                    {versionKey: false});
-
-const User = mongoose.model("user", userScheme); 
-
-
-    // Connect to MONGO DB  and create server------------------------------
-mongoose.connect(
-    "mongodb+srv://___your_momgodb_profile___", 
-    { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, 
-    function(err){
-    if(err) return console.log(err);
-    app.listen(5000, function(){
-        console.log("The server is running ...");
-    });
+  
+app.use(express.static(__dirname + "/public"));
+  
+const filePath = "users.json";
+app.get("/api/users", function(req, res){
+       
+    const content = fs.readFileSync(filePath,"utf8");
+    const users = JSON.parse(content);
+    res.send(users);
 });
-
-
-    // Upload form data from client -----------------------------------------
-const multer  = require('multer')
-const upload = multer({ dest: './data/uploads/' })
-
-app.post('/stats', upload.single('uploaded_file'), function (req, res) {
-   
-   console.log(req.file, req.body);
-
-    // edit user's photo
-   sharp(req.file.path)                    
-                    .resize(200)
-                    .toBuffer()
-                    .then( data => {
-                        editFileName = __dirname + '/data/' + req.body.surname + '.png';
-                        fs.writeFileSync(editFileName, data);
-                    })
-                    .then( () => {
-                        delFile = __dirname + '/data/uploads/' + req.file.filename;
-                        fs.unlink(delFile, function (err) {
-                            if (err) throw err;
-                                console.log('File deleted!');
-                        });
-                    })
-                    .catch( err => {
-                        console.log(err);
-                    });
-
-    
-      // add user's data to Mongo db  
-    const userName = req.body.name;
-    const userSurname = req.body.surname;
-    const userEmail = req.body.email;
-    const userPhoto = req.body.surname + '.png';    
-
-    const user = new User({ name: userName,
-                            surname: userSurname,
-                            email: userEmail,
-                            photo: userPhoto });
-        
-    user.save(function(err){
-        if(err) return console.log(err);
-        console.log(user)
-        res.send('<h3>user id - ' + user._id + '</h3>'); // send user's id to client
-    });
-
-                
-});
-
-
-
-    // All users -------------------------------------
-
-app.get("/api/user", function(req, res){
-        
-    User.find({}, function(err, users){
- 
-        if(err) return console.log(err);
-        res.send(users)
-    });
-});
-
-    // Get user by id-------------------------------------
-app.get("/api/user/:id", function(req, res){
-         
-    const id = req.params.id;
-    User.findOne({_id: id}, function(err, user){
-          
-        if(err) return console.log(err);
-
+// получение одного пользователя по id
+app.get("/api/users/:id", function(req, res){
+       
+    const id = req.params.id; // получаем id
+    const content = fs.readFileSync(filePath, "utf8");
+    const users = JSON.parse(content);
+    let user = null;
+    // находим в массиве пользователя по id
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==id){
+            user = users[i];
+            break;
+        }
+    }
+    // отправляем пользователя
+    if(user){
         res.send(user);
-    });
+    }
+    else{
+        res.status(404).send();
+    }
+});
+// получение отправленных данных
+app.post("/api/users", jsonParser, function (req, res) {
+      
+    if(!req.body) return res.sendStatus(400);
+      
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    let user = {name: userName, age: userAge};
+      
+    let data = fs.readFileSync(filePath, "utf8");
+    let users = JSON.parse(data);
+      
+    // находим максимальный id
+    const id = Math.max.apply(Math,users.map(function(o){return o.id;}))
+    // увеличиваем его на единицу
+    user.id = id+1;
+    // добавляем пользователя в массив
+    users.push(user);
+    data = JSON.stringify(users);
+    // перезаписываем файл с новыми данными
+    fs.writeFileSync("users.json", data);
+    res.send(user);
+});
+ // удаление пользователя по id
+app.delete("/api/users/:id", function(req, res){
+       
+    const id = req.params.id;
+    let data = fs.readFileSync(filePath, "utf8");
+    let users = JSON.parse(data);
+    let index = -1;
+    // находим индекс пользователя в массиве
+    for(var i=0; i < users.length; i++){
+        if(users[i].id==id){
+            index=i;
+            break;
+        }
+    }
+    if(index > -1){
+        // удаляем пользователя из массива по индексу
+        const user = users.splice(index, 1)[0];
+        data = JSON.stringify(users);
+        fs.writeFileSync("users.json", data);
+        // отправляем удаленного пользователя
+        res.send(user);
+    }
+    else{
+        res.status(404).send();
+    }
+});
+// изменение пользователя
+app.put("/api/users", jsonParser, function(req, res){
+       
+    if(!req.body) return res.sendStatus(400);
+      
+    const userId = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+      
+    let data = fs.readFileSync(filePath, "utf8");
+    const users = JSON.parse(data);
+    let user;
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==userId){
+            user = users[i];
+            break;
+        }
+    }
+    // изменяем данные у пользователя
+    if(user){
+        user.age = userAge;
+        user.name = userName;
+        data = JSON.stringify(users);
+        fs.writeFileSync("users.json", data);
+        res.send(user);
+    }
+    else{
+        res.status(404).send(user);
+    }
+});
+   
+app.listen(3000, function(){
+    console.log("Сервер ожидает подключения...");
 });
